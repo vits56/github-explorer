4f6941e529be024fff7f37487c2ae855cfcc4595
bastosduartebruno@gmail.com <bastosduartebruno@gmail.com>
2021-12-29 16:46:03 -0300

    importando css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
+import './styles/global.css';
+
 export function App() {
-    throw new Error('Eita Jeovvana o forninho caiu!!');
     return <h1>Hello React</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,13 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
-            }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
         ],
+
     }
 }
